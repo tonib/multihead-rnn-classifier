932fdbc3e98873585bca2f61e5b91f5f0825b94d
tonib <toni.bennasar@gmail.com>
2019-02-23 08:59:32 +0100

    Model server, handle exceptions

--- model_server.py
+++ model_server.py
@@ -2,6 +2,7 @@ from prediction_model import PredictionModel
 from model_data_definition import ModelDataDefinition
 import sys
 import json
+import traceback
 
 # Read data definition
 data_definition = ModelDataDefinition( 'data' )
@@ -15,8 +16,15 @@ print("READY TO SERVE")
 print("# Sample:", json.dumps( [data_definition.get_padding_element() ] * data_definition.sequence_length ) )
 
 while True:
-    # TODO: Handle exceptions
-    txt_line = sys.stdin.readline()
-    #print("# input_array: " + " ".join(str(x) for x in input_array))
-    result = predictor.predict_json(txt_line, data_definition)
-    sys.stdout.write( result + '\n')
+    try:
+        txt_line = sys.stdin.readline()
+        #print("# input_array: " + " ".join(str(x) for x in input_array))
+        result = predictor.predict_json(txt_line, data_definition)
+        sys.stdout.write( result + '\n')
+    except Exception as e:
+        # Return exception info
+        error_info = {}
+        error_info['type'] = 'error'
+        error_info['message'] = str(e)
+        error_info['stacktrace'] = traceback.format_exc()
+        sys.stdout.write( json.dumps(error_info) + '\n')
