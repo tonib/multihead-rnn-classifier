9d2f8b4bf949b6fbe157a356ae37cbb18ff20705
Toni Bennasar <toni.bennasar@gmail.com>
2021-02-27 10:12:24 +0100

    Sequences generation, added EOS symbol

--- test.py
+++ test.py
@@ -3,8 +3,7 @@ from tensorflow.data import Dataset
 
 sequence_length = 55
 sequence_feature_names = [ "sequence" ]
-context_feature_names = [ "sequence" ]
-output_feature_names = [ "output" ]
+eos = -1
 
 data = { 'sequence': list(range(1,11)), 'context': list(range(21, 31)) , 'output': list(range(31, 41)) }
 ds = Dataset.from_tensor_slices(data)
@@ -23,27 +22,30 @@ print("--", ds)
 def expand_first_sequence(window_elements_dict):
     """ Maps the first sequece to a dataset with initial incomplete subsequences. 
         Zero will be used for padding.
-        Ex (padding element is zero, sequence_length = 2): [1, 2, 3] -> { "in":[ [1, 0], [1, 2] ], "out": [ 2 , 3 ] ] """
+        Ex (padding element = 0, eos =-1, sequence_length = 2): 
+        [1, 2, 3] -> { "in":[ [-1, 0, 0] [1, -1, 0], [1, 2, -1] ], "out": [ 1, 2 , 3 ] ] } """
     expanded_sequence = {}
     for key in window_elements_dict:
-        feature_values = window_elements_dict[key] # [1, 2, 3]
         if key in sequence_feature_names:
+            inputs = window_elements_dict[key] # [1, 2, 3]
             # Sequence feature
-            inputs_length = tf.shape(feature_values)[0] - 1
-            inputs = feature_values[:-1] # [1, 2, 3] -> [1, 2]
-            inputs = tf.reshape(inputs, (1, -1)) # [1, 2] -> [[1, 2]]
-            inputs = tf.repeat(inputs, repeats=inputs_length, axis=0) # [[1, 2]] -> [[1, 2], [1, 2]]
-            expanded_sequence[key] = tf.linalg.band_part( inputs , inputs_length , 0 ) # Keep lower subdiagonals: [[1, 2], [1, 2]] -> [[1, 0], [1, 2]]
+            elements_length = tf.shape(inputs)[0]
+            inputs = tf.reshape(inputs, (1, -1)) # [1, 2, 3] -> [[1, 2, 3]]
+            inputs = tf.repeat(inputs, repeats=elements_length, axis=0) # [[1, 2, 3]] -> [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+            # Keep lower subdiagonals: [[1, 2, 3], [1, 2, 3], [1, 2, 3]] -> [[1, 0, 0], [1, 2, 0], [1, 2, 3]]
+            inputs = tf.linalg.band_part( inputs , elements_length , 0 )
+            # Assign EOS: [[1, 0, 0], [1, 2, 0], [1, 2, 3]] -> [[-1, 0, 0], [1, -1, 0], [1, 2, -1]]
+            eos_vector = tf.repeat( eos, elements_length)
+            expanded_sequence[key] = tf.linalg.set_diag(inputs, eos_vector)
         else:
-            # Context or output
-            expanded_sequence[key] = feature_values[1:] # [1, 2, 3] -> [2, 3]
+            expanded_sequence[key] = window_elements_dict[key]
     
     #return Dataset.from_tensor_slices(expanded_sequence)
     return expanded_sequence
 
 first_element_ds = ds.take(1)
 first_element_ds = first_element_ds.map(expand_first_sequence)
-#first_element_ds = first_element_ds.flat_map(lambda x: Dataset.from_tensor_slices(x))
+first_element_ds = first_element_ds.flat_map(lambda x: Dataset.from_tensor_slices(x))
 
 #ds = first_element_ds.concatenate( ds.skip(1) )
 ds = first_element_ds
