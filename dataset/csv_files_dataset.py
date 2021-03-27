e343e61cab2ed2642d29a2a112109d2a84f54660
Toni Bennasar <toni.bennasar@gmail.com>
2021-03-27 11:26:33 +0100

    Dataset: Refactoring to separate csv sequences generation and sequences processing

---
+++ dataset/csv_files_dataset.py
@@ -0,0 +1,119 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Tuple
+if TYPE_CHECKING:
+    from ..model_data_definition import ModelDataDefinition
+    from ..data_directory import DataDirectory
+
+import tensorflow as tf
+
+class CsvFilesDataset:
+
+    # CSV files separator character
+    CSV_SEPARATOR = ";"
+
+    # Key in dataset dictionary for file path
+    FILE_KEY = '_file_path'
+
+    # Key in dataset dictionary for row column
+    ROW_KEY = '_file_row'
+
+    def __init__(self, csv_files: DataDirectory, data_definition: ModelDataDefinition, shuffle: bool, debug_columns: bool=False):
+
+        self._csv_files = csv_files
+        self._data_definition = data_definition
+        self._get_csv_files_structure()
+        self.debug_columns = debug_columns
+        self.shuffle = shuffle
+
+        self.context_columns = list(data_definition.context_columns)
+        if debug_columns:
+            self.context_columns.append(CsvFilesDataset.FILE_KEY)
+            self.context_columns.append(CsvFilesDataset.ROW_KEY)
+        if data_definition.trainable_column:
+            self.context_columns.append(data_definition.trainable_column)
+
+        # Get entire CSV files in pipeline, as a dictionary, key=CSV column name, value=values in that column
+        self.dataset = tf.data.Dataset.list_files(csv_files.file_paths, shuffle=shuffle)
+
+        # Get a CSV dataset for each CSV file path
+        self.dataset = self.dataset.interleave(self._load_csv,
+            cycle_length=1 if not shuffle else 16,
+            #num_parallel_calls=None if not shuffle else tf.data.experimental.AUTOTUNE,
+            num_parallel_calls=None if not shuffle else 16,
+            deterministic=not shuffle
+        )
+        
+    def _load_csv(self, file_path):
+        """ Load full CSV file content and return it to the pipeline as dict with keys=column names, values=column values """
+        csv_ds = tf.data.experimental.CsvDataset(
+            file_path, self._default_csv_values,
+            header=True,
+            field_delim=CsvFilesDataset.CSV_SEPARATOR,
+            use_quote_delim=False,
+            select_cols=self._feature_column_indices
+        )
+
+        # Map to dictionary with column names
+        if self.debug_columns:
+            csv_ds = csv_ds.enumerate()
+            csv_ds = csv_ds.map(lambda *row: self._map_csv_row_to_dict_with_debug(file_path, row))
+        else:
+            csv_ds = csv_ds.map(
+                lambda *row: { feature_column_name: csv_column_values for feature_column_name, csv_column_values in zip(self._feature_column_names, row) }
+            )
+
+        # Get CSV file sequences
+        csv_ds = self._map_csv_file_to_sequences(csv_ds)
+
+        # Remove train column (avoid keras warning about unused inputs)
+        if self._data_definition.trainable_column:
+            csv_ds = csv_ds.map(self.remove_trainable_column)
+
+        return csv_ds
+
+    def remove_trainable_column(self, input, output):
+        del input[self._data_definition.trainable_column]
+        return (input, output)
+
+    def _map_csv_row_to_dict_with_debug(self, file_path, enumerated_row):
+        row_dict = { feature_column_name: csv_column_values for feature_column_name, csv_column_values in zip(self._feature_column_names, enumerated_row[1]) }
+
+        row_dict[CsvFilesDataset.FILE_KEY] = file_path
+        row_dict[CsvFilesDataset.ROW_KEY] = enumerated_row[0] + 2
+        return row_dict
+
+    def _flat_map_window(self, window_elements_dict):
+        """ Get real window values. I don't really understand this step, but it's required """
+        result = {}
+        for key in window_elements_dict:
+            # See https://github.com/tensorflow/tensorflow/issues/23581#issuecomment-529702702
+            result[key] = tf.data.experimental.get_single_element( window_elements_dict[key].batch(self._data_definition.sequence_length) )
+        #return result
+        return tf.data.Dataset.from_tensors(result)
+
+    def _map_csv_file_to_sequences(self, csv_columns_dict) -> tf.data.Dataset:
+        """ Map a full csv file to windows of sequence_length elements """
+        # We NEED drop_remainder=False, but it's tricky. If the entire csv is smaller than sequence_length, if
+        # drop_remainder=True, the entire csv sequence will be dropped, and we don't what that. But, if drop_remainder=False,
+        # final sequences with length < sequence_length will be feeded, and they must to be filtered... ¯\_(ツ)_/¯
+        windows_ds = csv_columns_dict.window(self._data_definition.sequence_length, shift=1, drop_remainder=False)
+        windows_ds = windows_ds.flat_map(self._flat_map_window)
+        return windows_ds
+    
+    def _get_csv_files_structure(self):
+
+        self._feature_column_names = list( self._data_definition.get_column_names() )
+
+        # Tricky things: To get right sequences we must separate CSV contents, and seems not supported by TF CSV high level helpers
+        # So, guess the the CSV structure (all files MUST share the same structure):
+        with open(self._csv_files.file_paths[0]) as f:
+            first_line = f.readline()
+            csv_column_names = first_line.split(CsvFilesDataset.CSV_SEPARATOR)
+            csv_column_names_to_indices = { name:index for index, name in enumerate(csv_column_names) }
+            # select_cols parm in tf.data.experimental.CsvDataset must be ordered by column index. So, reorder feature_column_names
+            # to follow that order
+            self._feature_column_names.sort( key=lambda x: csv_column_names_to_indices[x] )
+            self._feature_column_indices = [ csv_column_names_to_indices[feature_column_name] for feature_column_name in self._feature_column_names ]
+
+        # Column types: All int32
+        self._default_csv_values = [ tf.int32 ] * len( self._feature_column_names )
--- dataset/rnn_dataset.py
+++ dataset/rnn_dataset.py
@@ -5,6 +5,7 @@ if TYPE_CHECKING:
     from ..data_directory import DataDirectory
 
 import tensorflow as tf
+from .csv_files_dataset import CsvFilesDataset
 
 # 118400
 # Batches: 1850 Elements: 118400 Time (s): 43.615002155303955 Elements/s: 2714.662252644221
@@ -15,7 +16,7 @@ import tensorflow as tf
 # 124800
 # Batches: 1950 Elements: 124800 Time (s): 45.92690873146057 Elements/s: 2717.3612038580395
 
-class RnnDataset:
+class RnnDataset(CsvFilesDataset):
 
     # Padding value. MUST TO BE ZERO (implementation details)
     PADDING_VALUE = 0
@@ -26,100 +27,19 @@ class RnnDataset:
     # Values 0 and 1 are "keywords"
     N_KEYWORD_VALUES = 2
 
-    # CSV files separator character
-    CSV_SEPARATOR = ";"
-
-    # Key in dataset dictionary for file path
-    FILE_KEY = '_file_path'
-
-    # Key in dataset dictionary for row column
-    ROW_KEY = '_file_row'
-
     def __init__(self, csv_files: DataDirectory, data_definition: ModelDataDefinition, shuffle: bool, debug_columns: bool=False):
-
-        self._csv_files = csv_files
-        self._data_definition = data_definition
-        self._get_csv_files_structure()
-        self.debug_columns = debug_columns
-        self.shuffle = shuffle
-
-        self.context_columns = list(data_definition.context_columns)
-        if debug_columns:
-            self.context_columns.append(RnnDataset.FILE_KEY)
-            self.context_columns.append(RnnDataset.ROW_KEY)
-        if data_definition.trainable_column:
-            self.context_columns.append(data_definition.trainable_column)
-
-        # Get entire CSV files in pipeline, as a dictionary, key=CSV column name, value=values in that column
-        self.dataset = tf.data.Dataset.list_files(csv_files.file_paths, shuffle=shuffle)
-
-        # Get a CSV dataset for each CSV file path
-        self.dataset = self.dataset.interleave(self._load_csv,
-            cycle_length=1 if not shuffle else 16,
-            #num_parallel_calls=None if not shuffle else tf.data.experimental.AUTOTUNE,
-            num_parallel_calls=None if not shuffle else 16,
-            deterministic=not shuffle
-        )
+        super().__init__(csv_files, data_definition, shuffle, debug_columns)
         
-    def _load_csv(self, file_path):
-        """ Load full CSV file content and return it to the pipeline as dict with keys=column names, values=column values """
-        csv_ds = tf.data.experimental.CsvDataset(
-            file_path, self._default_csv_values,
-            header=True,
-            field_delim=RnnDataset.CSV_SEPARATOR,
-            use_quote_delim=False,
-            select_cols=self._feature_column_indices
-        )
-
-        # Map to dictionary with column names
-        if self.debug_columns:
-            csv_ds = csv_ds.enumerate()
-            csv_ds = csv_ds.map(lambda *row: self._map_csv_row_to_dict_with_debug(file_path, row))
-        else:
-            csv_ds = csv_ds.map(
-                lambda *row: { feature_column_name: csv_column_values for feature_column_name, csv_column_values in zip(self._feature_column_names, row) }
-            )
-
-        # Get CSV file sequences
-        csv_ds = self._map_csv_file_to_sequences(csv_ds)
-
-        # Process sequences
-        csv_ds = self._process_sequences(csv_ds)
-
-        # Remove train column (avoid keras warning about unused inputs)
-        if self._data_definition.trainable_column:
-            csv_ds = csv_ds.map(self.remove_trainable_column)
-
-        return csv_ds
-
-    def remove_trainable_column(self, input, output):
-        del input[self._data_definition.trainable_column]
-        return (input, output)
-
-    def _map_csv_row_to_dict_with_debug(self, file_path, enumerated_row):
-        row_dict = { feature_column_name: csv_column_values for feature_column_name, csv_column_values in zip(self._feature_column_names, enumerated_row[1]) }
-
-        row_dict[RnnDataset.FILE_KEY] = file_path
-        row_dict[RnnDataset.ROW_KEY] = enumerated_row[0] + 2
-        return row_dict
-
-    def _flat_map_window(self, window_elements_dict):
-        """ Get real window values. I don't really understand this step, but it's required """
-        result = {}
-        for key in window_elements_dict:
-            # See https://github.com/tensorflow/tensorflow/issues/23581#issuecomment-529702702
-            result[key] = tf.data.experimental.get_single_element( window_elements_dict[key].batch(self._data_definition.sequence_length) )
-        #return result
-        return tf.data.Dataset.from_tensors(result)
-
     def _map_csv_file_to_sequences(self, csv_columns_dict) -> tf.data.Dataset:
         """ Map a full csv file to windows of sequence_length elements """
-        # We NEED drop_remainder=False, but it's tricky. If the entire csv is smaller than sequence_length, if
-        # drop_remainder=True, the entire csv sequence will be dropped, and we don't what that. But, if drop_remainder=False,
-        # final sequences with length < sequence_length will be feeded, and they must to be filtered... ¯\_(ツ)_/¯
-        windows_ds = csv_columns_dict.window(self._data_definition.sequence_length, shift=1, drop_remainder=False)
-        windows_ds = windows_ds.flat_map(self._flat_map_window)
-        return windows_ds
+
+        # Get the csv file window sequences
+        csv_ds = CsvFilesDataset._map_csv_file_to_sequences(self, csv_columns_dict)
+
+        # Process window sequences
+        csv_ds = self._process_sequences(csv_ds)
+
+        return csv_ds
     
     def _process_sequences(self, windows_ds):
 
@@ -204,21 +124,3 @@ class RnnDataset:
             output_dict[key] = window_elements_dict[key][-1]
 
         return (input_dict, output_dict)
-
-    def _get_csv_files_structure(self):
-
-        self._feature_column_names = list( self._data_definition.get_column_names() )
-
-        # Tricky things: To get right sequences we must separate CSV contents, and seems not supported by TF CSV high level helpers
-        # So, guess the the CSV structure (all files MUST share the same structure):
-        with open(self._csv_files.file_paths[0]) as f:
-            first_line = f.readline()
-            csv_column_names = first_line.split(RnnDataset.CSV_SEPARATOR)
-            csv_column_names_to_indices = { name:index for index, name in enumerate(csv_column_names) }
-            # select_cols parm in tf.data.experimental.CsvDataset must be ordered by column index. So, reorder feature_column_names
-            # to follow that order
-            self._feature_column_names.sort( key=lambda x: csv_column_names_to_indices[x] )
-            self._feature_column_indices = [ csv_column_names_to_indices[feature_column_name] for feature_column_name in self._feature_column_names ]
-
-        # Column types: All int32
-        self._default_csv_values = [ tf.int32 ] * len( self._feature_column_names )
