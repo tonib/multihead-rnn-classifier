2be9d153811e6523cf7746437d4a398fd7d50098
Toni Bennasar <toni.bennasar@gmail.com>
2021-04-01 10:33:52 +0200

    Fixed error exporting model for GPT (not build model)

--- export.py
+++ export.py
@@ -1,5 +1,6 @@
 from model_data_definition import ModelDataDefinition
 from model_definition import ModelDefinition
+from data_directory import DataDirectory
 import tensorflow as tf
 import os
 
@@ -19,15 +20,23 @@ else:
     export_cp_path = checkpoints_dir + "/checkpoint-{0:04d}.ckpt".format( model_definition.data_definition.export_checkpoint )
     print("Export specific checkpoint", export_cp_path)
 
-print("Exporting model from checkpoint " + export_cp_path)
+print("Loading checkpoint " + export_cp_path)
 model = model_definition.create_model_function(model_definition.data_definition)
 model.load_weights(export_cp_path)
 
 # TODO: Fails with GPT model (missing signature?)
 # See https://stackoverflow.com/questions/51806852/cant-save-custom-subclassed-model
 # From previous, see: https://colab.research.google.com/drive/172D4jishSgE3N7AO6U2OKAA_0wNnrMOq#scrollTo=4Onp-8rGyeQG
+# The only way to build a subclassed keras model I have found is to run predictions. So, here is:
+print("Building model...")
+all_data = DataDirectory.read_all(model_definition.data_definition)
+ds = model_definition.dataset_class(all_data, model_definition.data_definition, shuffle=False, debug_columns=False)
+build_model_ds = ds.dataset.batch(1).take(1)
+for input, output in build_model_ds:
+    model(input)
 
 # Save full model
+print("Saving model...")
 exported_model_dir = model_definition.data_definition.get_data_dir_path( ModelDataDefinition.EXPORTED_MODEL_DIR )
 model.save( exported_model_dir, include_optimizer=False )
 print("Model exported to " + exported_model_dir)
