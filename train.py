566624251fa8efd1820dcf0cd5ba3430d8618ab2
Toni Bennasar <toni.bennasar@gmail.com>
2021-04-03 22:13:58 +0200

    Train: Allow to debug exported model will predict the same than trained model

--- train.py
+++ train.py
@@ -1,3 +1,9 @@
 from model_definition import ModelDefinition
 
-ModelDefinition().trainer_class().train()
+model_definition = ModelDefinition()
+trainer = model_definition.trainer_class()
+trainer.train()
+
+# Get prediction now to test if after export the prediction will be the same
+# predictor = model_definition.predictor_class(model_definition.data_definition, trainer.model)
+# print( "Prediction:" , predictor.predict( predictor.get_empty_element() ) )
