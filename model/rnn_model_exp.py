15aeef1ea0ddc351c3424233f6807038cd9363b2
Toni Bennasar <toni.bennasar@gmail.com>
2022-10-06 18:33:53 +0200

    Support for shared labels in rnn_model_exp model

--- model/rnn_model_exp.py
+++ model/rnn_model_exp.py
@@ -8,32 +8,48 @@ if TYPE_CHECKING:
 import tensorflow as tf
 from .masked_one_hot_encoding import MaskedOneHotEncoding
 
-def _get_input(data_definition: ModelDataDefinition, column_name: str, model_inputs: dict):
-
-    column_info: ColumnInfo  = data_definition.column_definitions[column_name]
-    input = tf.keras.Input(name=column_name, dtype=tf.int32, shape=[data_definition.sequence_length])
-
+def _get_preprocess_input_layer(column_info: ColumnInfo, column_name: str) -> tf.keras.layers.Layer:
     n_labels = len(column_info.labels) + RnnDatasetExp.N_KEYWORD_VALUES
-
     # Encode input, add masking
     if column_info.embeddable_dimension > 0:
-        processed_input = tf.keras.layers.Embedding(n_labels, column_info.embeddable_dimension, mask_zero=True, 
-            name="embedding_" + column_name)(input)
+        return tf.keras.layers.Embedding(n_labels, column_info.embeddable_dimension, mask_zero=True, 
+            name="embedding_" + column_name)
     else:
-        processed_input = MaskedOneHotEncoding(n_labels, name='one_hot_' + column_name)(input)
+        return MaskedOneHotEncoding(n_labels, name='one_hot_' + column_name)
+
+
+def _get_input(data_definition: ModelDataDefinition, column_name: str, model_inputs: dict, 
+    shared_preprocessing_layers : dict[str, tf.keras.layers.Layer]):
+
+    column_info: ColumnInfo = data_definition.column_definitions[column_name]
+    input = tf.keras.Input(name=column_name, dtype=tf.int32, shape=[data_definition.sequence_length])
 
     # This is the dict of Keras model inputs
     model_inputs[column_name] = input
 
-    return processed_input
+    if column_info.shared_labels_name != None:
+        # This column uses a shared labels set. All inputs using this shared set will use the same encoder:
+        preprocess_input_layer = shared_preprocessing_layers[column_info.shared_labels_name]
+    else:
+        # Create a custom encoder for this input
+        preprocess_input_layer = _get_preprocess_input_layer(column_info, column_name)
+
+    # return encoded input
+    return preprocess_input_layer(input)
+
 
 def create_rnn_model_exp(data_definition: ModelDataDefinition):
+
+    # Define shared preprocessing layers
+    shared_preprocessing_layers : dict[str, tf.keras.layers.Layer] = {} 
+    for shared_labels in data_definition.shared_labels.values():
+        shared_preprocessing_layers[shared_labels.name] = _get_preprocess_input_layer(shared_labels, shared_labels.name)
 
     # Define sequence inputs
     model_inputs = {}
     sequences = []
     for column_name in ( data_definition.sequence_columns + data_definition.context_columns ):
-        sequences.append( _get_input(data_definition, column_name, model_inputs) )
+        sequences.append( _get_input(data_definition, column_name, model_inputs, shared_preprocessing_layers) )
     
     # Merge all inputs for each timestep
     if len(sequences) > 1:
